71ef935a7a4186f3fa0027e669a26f798e9b34bf
Braden Lawrence <lawrencebradenm@gmail.com>
2017-02-27 20:48:28 -0500

    Adding functionality design goals

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Add button toggles entry form visibility
+
+
+// Pressing "Enter" on entry form creates a new todo item with the form's current text
+
+
+
+// Mouse hover on todo item toggles delete button visibility
+
+
+
+// Mouse click on todo item toggles text strikethrough
+
+
+
+// Mouse click on delete button removes todo item
