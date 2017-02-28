90828960b4183fe85f8fa7d10b676eb88b31d34c
Braden Lawrence <lawrencebradenm@gmail.com>
2017-02-27 22:51:40 -0500

    Adding event handler for the entry form
     - Created a todo object to manage the items added to the list
     - Created a todoItem constructor object. The todo.list array will consist of a bunch of these objects
     - Created a todo.add method. This accepts a text argument and pushes a new todoItem to todo.list array
     - Created an event handler for the entry form. When the user presses 'Enter', it passes the entry form's value to the todo.add method.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
+// Initialize list
+let todo = {
+  list: [],
+  add: function(text) {
+    this.list.push(new todoItem(text))
+  }
+}
+
+const todoItem = function(text) {
+  this.text = text
+}
+
 // Add button toggles entry form visibility
 let add = $(".add")
 let formControl = $(".form-control")
 
-add.click(function(){
-  formControl.slideToggle(200)
+add.click(function() {
+    formControl.slideToggle(200)
 })
 
 // Pressing "Enter" on entry form creates a new todo item with the form's current text
-
-
+formControl.keypress(function(event) {
+    if(event.which === 13) {    //13 === the 'Enter' key
+        todo.add(formControl.val())
+    }
+})
 
 // Mouse hover on todo item toggles delete button visibility
 
