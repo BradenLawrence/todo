7d4a634843f64b9ae345c4169b43be1c6175ef02
Braden Lawrence <lawrencebradenm@gmail.com>
2017-02-28 23:37:11 -0500

    Adding click handler to todoItems. Clicking on them now crosses them out.
     - Rewrote how list items are added to the list
        - The html li now has an id to let us refer to it later
        - the todo.display method now accepts a number argument. This is used to identify the li, and the index of the todo.list array.
     - The todo.display method now adds listeners to the newly added li
        - Currently the only available listener is a click handler to toggle the completed state of the todoItem

--- js/script.js
+++ js/script.js
@@ -1,19 +1,29 @@
 // Initialize list
 let todo = {
-  list: [],
-  interface: $(".listArea"),
-  display: function(object) {
-    this.interface.append("<li><span class=\"delete\"><i class=\"fa fa-minus-circle\" aria-hidden=\"true\"></i></span><span class=\"todoItem\">" + object.text + "</span></li>")
-  },
-  add: function(text) {
-    let item = new todoItem(text)
-    this.list.push(item)
-    this.display(item)
-  }
+    list: [],
+    interface: $(".listArea"),
+    add: function(text) {
+        let item = new todoItem(text)
+        this.list.push(item)
+        let newestID = this.list.length-1
+        this.display(newestID)   // Add the item we just created to the display list
+    },
+    display: function(num) {
+        let text = this.list[num].text
+        this.interface.append("<li id=\"todo" + num +  "\"><span class=\"delete hidden\"><i class=\"fa fa-minus-circle\" aria-hidden=\"true\"></i></span><span class=\"todoItem\">" + text + "</span></li>")
+        this.addListeners(num)
+    },
+    addListeners: function(num) {
+        let id = "#todo" + num
+        // Mouse click on todo item toggles text strikethrough
+        $(id).click(function() {
+            $(id).toggleClass("complete")
+        })
+    },
 }
 
 const todoItem = function(text) {
-  this.text = text
+    this.text = text
 }
 
 // Add button toggles entry form visibility
@@ -26,16 +36,12 @@ add.click(function() {
 
 // Pressing "Enter" on entry form creates a new todo item with the form's current text
 formControl.keypress(function(event) {
-    if(event.which === 13) {    //13 === the 'Enter' key
+    if (event.which === 13) { //13 === the 'Enter' key
         todo.add(formControl.val())
     }
 })
 
 // Mouse hover on todo item toggles delete button visibility
-
-
-
-// Mouse click on todo item toggles text strikethrough
 
 
 
