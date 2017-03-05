b73e5610a9c64f530f47cda9234f2b22929bdcef
Braden Lawrence <lawrencebradenm@gmail.com>
2017-03-04 22:29:15 -0500

    Adding quality of life improvements.
     - Text entry form now clears the default text when it gains focus, and replaces it when losing focus
     - Entering a todo item now clears the text entry box so it is ready to begin typing a new entry.

--- js/script.js
+++ js/script.js
@@ -5,11 +5,11 @@ let todo = {
     add: function(text) {
         let item = new todoItem(text)
         this.list.push(item)
-        let newestID = this.list.length-1
-        this.display(newestID)   // Add the item we just created to the display list
+        this.display(item.id, item.text)
+        // let newestID = this.list.length-1
+        // this.display(newestID)   // Add the item we just created to the display list
     },
-    display: function(num) {
-        let text = this.list[num].text
+    display: function(num, text) {
         this.interface.append("<li id=\"todo" + num +  "\"><span class=\"delete hidden\"><i class=\"fa fa-minus-circle\" aria-hidden=\"true\"></i></span><span class=\"todoItem\">" + text + "</span></li>")
         this.addListeners(num)
     },
@@ -18,6 +18,7 @@ let todo = {
         // Mouse click on todo item toggles text strikethrough
         $(id).click(function() {
             $(id).toggleClass("complete")
+            todo.list[num].setStatus("complete")
         })
         // Mouse hover on todo item toggles delete button visibility
         $(id).hover(function() {      // Mouse enter
@@ -30,13 +31,23 @@ let todo = {
         $(id).children("span.delete").click(function() {
             $(id).slideUp(200, function() {
                   $(id).remove()
+                  todo.list[num].setStatus("removed")
             })
         })
     }
 }
 
+let todoCounter = 0
 const todoItem = function(text) {
     this.text = text
+    this.status = "incomplete"
+    this.id = todoCounter
+    todoCounter++
+    this.setStatus = function(text) {
+        if(text === "complete" || text === "incomplete" || text === "removed") {
+            this.status = text
+        }
+    }
 }
 
 // Add button toggles entry form visibility
@@ -51,5 +62,19 @@ add.click(function() {
 formControl.keypress(function(event) {
     if (event.which === 13) { //13 === the 'Enter' key
         todo.add(formControl.val())
+        formControl.val("")
     }
 })
+
+// Clicking on the entry form clears default text
+formControl.focusin(function() {
+    formControl.val("")
+})
+
+formControl.focusout(function() {
+    initializeForm()
+})
+
+const initializeForm = function() {
+    formControl.val("Enter your task...")
+}
