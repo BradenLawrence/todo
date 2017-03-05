aa20be07cec73caa6a9f0db665984392a01ceef4
Braden Lawrence <lawrencebradenm@gmail.com>
2017-03-04 23:48:21 -0500

    Adding delete button hover animation
     - Removing old event listener from javaScript
     - Delete button visibility is now controlled by css
        - li:hover .delete
        - transition effects now animate the button's visibility, size, and position.

--- js/script.js
+++ js/script.js
@@ -6,11 +6,9 @@ let todo = {
         let item = new todoItem(text)
         this.list.push(item)
         this.display(item.id, item.text)
-        // let newestID = this.list.length-1
-        // this.display(newestID)   // Add the item we just created to the display list
     },
     display: function(num, text) {
-        this.interface.append("<li id=\"todo" + num + "\"><span class=\"delete hidden\"><i class=\"fa fa-minus-circle\" aria-hidden=\"true\"></i></span><span class=\"todoItem\">" + text + "</span></li>")
+        this.interface.append("<li id=\"todo" + num + "\"><span class=\"delete\"><i class=\"fa fa-minus-circle\" aria-hidden=\"true\"></i></span><span class=\"todoItem\">" + text + "</span></li>")
         this.addListeners(num)
     },
     addListeners: function(num) {
@@ -20,13 +18,6 @@ let todo = {
             $(id).toggleClass("complete")
             todo.list[num].setStatus("complete")
         })
-        // Mouse hover on todo item toggles delete button visibility
-        $(id).hover(function() { // Mouse enter
-                $(id).children("span.delete").toggleClass("hidden")
-            },
-            function() { // Mouse leave
-                $(id).children("span.delete").toggleClass("hidden")
-            })
         // Mouse click on delete button removes todo item
         $(id).children("span.delete").click(function() {
             $(id).slideUp(200, function() {
